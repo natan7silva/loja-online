78d2d74ad6e5d0680d94b9ae27ce2fa6bfa618bf
natan7silva <natan4silva143@gmail.com>
2026-09-07 11:36:28 -0300

    docs: comentario na funcao calcular total

---
+++ app.js
@@ -0,0 +1,19 @@
+
+
+
+function calcularTotal(itens) {
+
+   let total = 0
+
+   for (let i = 0; i < itens.length; i++) {
+
+       total += itens[i].preco
+   }
+
+   return total
+
+   // aplica desconto de fidelidade
+   // antes de retornar o valor final
+
+
+}
